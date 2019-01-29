b9a654eb37b0d920445d298da75ced1d8daed134
Omar AlArifi <omar@Omars-MacBook-Air.local>
2019-01-29 23:10:22 +0300

    Created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
