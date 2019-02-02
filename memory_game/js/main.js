601b1aac0d26bdc530ac605e866dcc69e3470c10
Omar AlArifi <insert github email here>
2019-02-02 22:47:23 +0300

    created gaame logic and cleaned code

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,27 @@
 
 
 var cards = ['queen', 'queen','king', 'king'];
-	
+		
 var cardsInPlay = [];
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards [2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) { alert("You Found a Match!!!")}
-		else (alert("Sorry, try again."))
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
 }
+var flipCard = function (cardId) {
+	console.log("User Flipped " + cards[cardId]);
+	cardsInPlay.push(cardId);
+}	
+flipCard(0);
+flipCard(2);
 
-	
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) { alert("You Found a Match!!!")}
+			else (alert("Sorry, try again."))
+	}
+
+
+
+		
